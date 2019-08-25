d0165aab9c91912aa30dae431404bdecc0e3af43
sergeypavlyk <pavlyk.sergeynik@gmail.com>
2019-08-25 17:20:03 +0300

    banner1 completed

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    
+    
+
+    // $('.leftarrow').hover(function() {
+    //     $('.leftarrow_click').addClass('active').fadeIn(500);
+    //     event.stopPropagation();
+    // },function(){
+    //     $('.leftarrow_click').fadeOut(500);
+    // });
+    // $('.rightarrow').hover(function() {
+    //     $('.rightarrow_click').addClass('active').fadeIn(500);
+    // },function(){
+    //     $('.rightarrow_click').fadeOut(500);
+    // });
+    
+    new WOW().init();
+});
